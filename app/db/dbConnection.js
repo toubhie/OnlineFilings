423928754047d1623a0140b7a222de196478ce1b
toubhie <tobiwily@yahoo.com>
2023-04-15 06:54:43 +0100

    Update dbConnection.js

--- app/db/dbConnection.js
+++ app/db/dbConnection.js
@@ -4,9 +4,8 @@ import constants from "../utils/constants";
 
 const client = new MongoClient(constants.mongoDBUri); // Mongo Client
 
-/*
-  init method is to connect with mongo database. 
-*/
+
+// init method is to connect with mongo database. 
 const initMongoDBConnection = async () => {
   try {
     //connect to the local mongoDB 
@@ -14,26 +13,29 @@ const initMongoDBConnection = async () => {
     console.log(`Connected To Database ${env.database}`);
   } catch (error) {
     console.log(error);
+    throw new Error("Cannot connect to Mongo DB");
   }
 };
 
-/*
-  closeConnection method is used to end connection with the database. 
-*/
+// closeConnection method is used to end connection with the database. 
 const endMongoConnection = async () => {
   try {
     await client.close();
     console.log('DB connection closed');
   } catch (error) {
     console.log(error);
+    throw new Error("Mongo DB connection failed to close");
   }
 };
 
-/*
-  getClient Method is used to get the database client.
-*/
+// getClient Method is used to get the database client.
 const getClient = () => {
-  return client.db(env.database); // selecting the db
+  try {
+    return client.db(env.database); // selecting the db
+  } catch (error) {
+    console.log(error);
+  }
+
 };
 
 export {
